66b9f707e59eaa8342dad1c730095e60c49dc957
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-12-07 08:49:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool car (char &a) {
+    bool var = false ;
+    if((a>64) and (a<91)){
+        a=a+32 ;
+        var=true;
+    }else if ((a>96) and (a<123)){
+        a=a-32;
+        var=true;
+    }else {
+        var=false;
+    }return var;
+}
